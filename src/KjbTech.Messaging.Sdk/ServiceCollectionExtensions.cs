16e7e5beade4966cde54e640bc4f175618d88656
KevinB <67744786+KevinBTech@users.noreply.github.com>
2025-10-20 12:22:02 +0200

    refactor

--- src/KjbTech.Messaging.Sdk/ServiceCollectionExtensions.cs
+++ src/KjbTech.Messaging.Sdk/ServiceCollectionExtensions.cs
@@ -4,7 +4,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using System.Net.Http.Headers;
-using System.Reflection;
 
 namespace KjbTech.Messaging.Sdk;
 
